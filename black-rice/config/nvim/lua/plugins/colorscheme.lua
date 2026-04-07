93d0738874c87bf7209d198f3b838611f7f81f06
Arvin Garcia <arvin@arvingarcia.com>
2026-04-07 18:09:59 +0800

    feat: use vague.nvim colorscheme

--- black-rice/config/nvim/lua/plugins/colorscheme.lua
+++ black-rice/config/nvim/lua/plugins/colorscheme.lua
@@ -12,15 +12,15 @@ return
 -- }
 --
 -- ***koda.nvim***
-{
-  "oskarnurm/koda.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- require("koda").setup({ transparent = true })
-    vim.cmd("colorscheme koda")
-  end,
-}
+-- {
+--   "oskarnurm/koda.nvim",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     -- require("koda").setup({ transparent = true })
+--     vim.cmd("colorscheme koda")
+--   end,
+-- }
 -- *** monochrome.nvim ***
 -- {
 --   { "kdheepak/monochrome.nvim" },
@@ -32,3 +32,46 @@ return
 --     },
 --   },
 -- }
+--
+-- ***zenbones.nvim***
+-- {
+--   "zenbones-theme/zenbones.nvim",
+--   -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+--   -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+--   -- In Vim, compat mode is turned on as Lush only works in Neovim.
+--   dependencies = "rktjmp/lush.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   -- you can set set configuration options here
+--   -- config = function()
+--   --     vim.g.zenbones_darken_comments = 45
+--   --     vim.cmd.colorscheme('zenbones')
+--   -- end
+-- }
+--
+-- ***lackluster.nvim***
+-- {
+--   "slugbyte/lackluster.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   init = function()
+--     vim.cmd.colorscheme("lackluster")
+--     -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+--     -- vim.cmd.colorscheme("lackluster-mint")
+--   end,
+-- }
+{
+  "vague-theme/vague.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other plugins
+  config = function()
+    -- NOTE: you do not need to call setup if you don't want to.
+    require("vague").setup({
+      -- optional configuration here
+      colors = {
+        bg = "#101010",
+      },
+    })
+    vim.cmd("colorscheme vague")
+  end,
+}
